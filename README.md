6289d9c48f4bbafdfbdfc2dd3e69d1803e2552d9
David Miller <dlm6693@gmail.com>
2020-03-02 17:48:43 -0500

    finished project

---
+++ README.md
@@ -0,0 +1,29 @@
+# Data Modeling with Postgres
+## By David Miller
+
+## How to Run the Python Scripts in Command Line
+1. First sure you've navigated to the directory where the project is located
+2. Run `python create_tables.py` to create the tables within the database
+3. Run `python etl.py` to grab, process and store the data in the tables you just created
+
+## Project Overview
+## Part I - Writing the Queries
+* Before doing anything else, writing the queries that create the tables and insert data into said tables, but also dropping them if they exist so as not to create duplicate tables is key
+* This is all done in `sql_queries.py`
+* It is all mostly written in raw SQL, but both the create and insert queries are stored within lists to easily iterate over later
+* All of the Python logic to create the tables was already put in place by Udacity in `create_tables.py`
+    * All its doing is just that creating and dropping the tables! The functions doing the work establish a connection to the database, executes the create and drop queries written in `sql_queries.py`, then commits those changes so that they're made permanent
+    * Then the `main` method calls the two defined functions above, and is actually run in the last line of the file
+
+## Part II - Extract, Transform and Load!
+* This is where the bulk of the project was done by me
+* Most of the initial QA, experimentaiton was done in the interactive notebook `etl.ipynb`, but later debugged in the command line
+    * A lot of the logic in here was prebuilt by Udacity, which I didn't change much. It was only later that I decided to rewrite code in `etl.py` when it became apparent there were issues the code template weren't handling very well
+* Essentially, the data is initially stored in several separate files, the're grabbed and combined to create two raw data tables
+* Then they're split into 5 separate tables for ingestion
+* Before doing this however, duplicates needed to be handled and some data/cleaning transformation was necessary too
+    * Time data needed to converted and pulled out to create the time table
+    * Additionally there was a userId column in one of the tables that had numbers stored as both strings and integers, which fooled a Pandas duplicates method, but not Postgres
+* In `etl.py` I decided to rework the logic written by Udacity to bulk load the data where possible instead of one by one. To do so I created a helper function to chunk the data so not too much would be thrown in at once
+    * Additionally each load function was rewritten to take in all of the filepaths where the data was located rather than just one. 
+    * This did not require as much rewriting as I thought it might!
---
+++ etl.py
@@ -0,0 +1,150 @@
+import os
+import glob
+import psycopg2
+import pandas as pd
+from sql_queries import *
+
+#helper function to chunk data for efficient loading
+def chunk_and_execute(cur, query, data, chunksize):
+    for i in range(0, len(data), chunksize):
+        chunk = data[i:i+chunksize]
+        cur.executemany(query, chunk)
+
+def process_song_files(cur, filepaths):
+    # open song file
+    dfs = []
+    #read each filepath into a dataframe then combine them all into one df
+    for path in filepaths:
+        dfa = pd.read_json(path, lines=True)
+        dfs.append(dfa)
+    df = pd.concat(dfs).reset_index(drop=True)
+    
+    # grab all artist values while dropping duplicates that arise in the raw data
+    artist_data = list(df[[
+                        'artist_id', 
+                        'artist_name', 
+                        'artist_location', 
+                        'artist_latitude', 
+                        'artist_longitude'
+                                    ]].drop_duplicates().values)
+    #insert 1000 artist records at once
+    chunk_and_execute(cur=cur, query=artist_table_insert, data=artist_data, chunksize=1000)
+    
+    # insert 1000 song records at once
+    song_data = list(df[['song_id', 'title', 'artist_id', 'year', 'duration']].drop_duplicates().values)
+    chunk_and_execute(cur=cur, query=song_table_insert, data=song_data, chunksize=1000)
+    
+    
+
+def process_log_files(cur, filepaths):
+    # open log file
+    dfs = []
+    for path in filepaths:
+        dfa = pd.read_json(path, lines=True)
+        dfs.append(dfa)
+    df = pd.concat(dfs).reset_index(drop=True)
+
+    # filter by NextSong action and dropping duplicates returned from raw data
+    df = df.query('page == "NextSong"')
+    df = df.drop_duplicates()
+    # convert timestamp column to datetime
+    df.ts = df.ts.apply(lambda x: pd.Timestamp(x, unit='ms'))
+    
+    # insert time data records
+    column_labels = [
+                    'timestamp',
+                    'hour_of_day', 
+                    'day_of_month', 
+                    'week_of_year', 
+                    'month_of_year', 
+                    'year', 
+                    'day_of_week'
+                                ]
+    #creating the time dataframe
+    time_df = pd.concat(
+                        [
+                        df['ts'], 
+                        df['ts'].dt.hour, 
+                        df['ts'].dt.day, 
+                        df['ts'].dt.week, 
+                        df['ts'].dt.month, 
+                        df['ts'].dt.year, 
+                        df['ts'].dt.weekday
+                            ], 
+                            axis=1, keys=column_labels)
+    time_values = list(time_df.values)
+    chunk_and_execute(cur=cur, query=time_table_insert, data=time_values, chunksize=1000)
+
+    # load user table
+    user_df = df[[
+                'userId', 
+                'firstName', 
+                'lastName', 
+                'gender', 
+                'level'
+                    ]]
+    user_df.userId = pd.to_numeric(user_df.userId)
+    user_df = user_df.drop_duplicates()
+    # insert user records
+    user_values = list(user_df.values)
+    chunk_and_execute(cur=cur, query=user_table_insert, data=user_values, chunksize=1000)
+    
+    # insert songplay records
+    #due to the complex nature of the overall query, one record has to be inserted at a time
+    for index, row in df.iterrows():
+        
+        # get songid and artistid from song and artist tables
+        cur.execute(song_select, (row.song, row.artist, row.length))
+        results = cur.fetchone()
+        
+        if results:
+            songid, artistid = results
+        else:
+            songid, artistid = None, None
+
+        # insert songplay record
+        songplay_data = [
+                            index, 
+                            row['ts'], 
+                            row['userId'], 
+                            row['level'], 
+                            songid, 
+                            artistid, 
+                            row['sessionId'], 
+                            row['location'], 
+                            row['userAgent']
+                                            ]
+        
+        cur.execute(songplay_table_insert, songplay_data)
+
+
+def process_data(cur, conn, filepath, func):
+    # get all files matching extension from directory
+    all_files = []
+    for root, dirs, files in os.walk(filepath):
+        files = glob.glob(os.path.join(root,'*.json'))
+        for f in files :
+            all_files.append(os.path.abspath(f))
+
+    # get total number of files found
+    num_files = len(all_files)
+    print('{} files found in {}'.format(num_files, filepath))
+
+    # iterate over files and process
+    func(cur=cur, filepaths=all_files)
+    conn.commit()
+    print('{} files processed.'.format(len(all_files)))
+
+
+def main():
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    cur = conn.cursor()
+
+    process_data(cur, conn, filepath='data/song_data', func=process_song_files)
+    process_data(cur, conn, filepath='data/log_data', func=process_log_files)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
